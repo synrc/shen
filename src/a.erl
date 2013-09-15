d824b4c4e44f7c38e0894a08338541681e6d799b
Maxim Sokhatsky <maxim@synrc.com>
2013-09-15 05:04:52 +0300

    stage

--- src/a.erl
+++ src/a.erl
@@ -12,16 +12,13 @@ doc_ready(E) ->
     D:ready(fun() -> T = jq("a[href=\"#" ++ E ++ "\"]"), T:tab("show") end).
 
 macro(A,B,C) ->
-    X = jq("document"),
-    E = fun(Y,Z) -> X:at("ss") end,
     ws:send('Bert':encodebuf(
         [{source,'Bert':binary(A)},
          {x,C},
          {pickle,'Bert':binary(B)},
          {linked,C}])).
 main() ->
-    A = "1",
-    B = "2",
-    Script1 = macro(A,B,"3"),
-    Script = doc_ready("'tab'"),
-    io:format("JS Macro: ~s",[Script]).
+    Script1 = tabshow(),
+    Script2 = macro("1","2","3"),
+    Script3 = doc_ready("tab"),
+    io:format("tabshow/0:~n~s~nevent/3:~n~s~ndoc_ready/1:~n~s~n",[Script1,Script2,Script3]).
