95d5aace66980a6570be757b2cbe490fa581b654
Maxim Sokhatsky <maxim@synrc.com>
2013-10-11 21:13:50 +0300

    import shen functions in modules

--- src/shen_lists.erl
+++ src/shen_lists.erl
@@ -1,8 +1,9 @@
 -module(shen_lists).
 -author('Maxim Sokhatsky').
 -copyright('Synrc Research Center').
+-import(shen,[exp/2]).
 -compile(export_all).
 
-map(Fun,List,Mode) -> io_lib:format("~s.map(~s);\n",[shen:exp(List,Mode),shen:exp(Fun,Mode)]).
-foldl(Fun,Acc,List,Mode) -> io_lib:format("~s.reduce(~s,~s);\n",[shen:exp(List,Mode),shen:exp(Fun,Mode),shen:exp(Acc,Mode)]).
+map(Fun,List,Mode) -> io_lib:format("~s.map(~s);\n",[exp(List,Mode),exp(Fun,Mode)]).
+foldl(Fun,Acc,List,Mode) -> io_lib:format("~s.reduce(~s,~s);\n",[exp(List,Mode),exp(Fun,Mode),exp(Acc,Mode)]).
 foldr(Fun,Acc,List,Mode) -> foldl(Fun,Acc,List,Mode).
